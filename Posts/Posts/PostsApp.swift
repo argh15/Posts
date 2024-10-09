b3f0c042cea41c2990960ce90655fa5d872be73b
Arghadeep <argho.chakr@gmail.com>
2024-10-09 18:57:09 -0400

    Initial Commit

---
+++ Posts/Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Argh on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
